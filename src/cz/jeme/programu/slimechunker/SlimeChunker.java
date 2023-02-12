e3479e1637982f7c9814ce5955924e73fb180311
XD_CZ <mandlemankiller@email.cz>
2023-02-12 12:28:51 +0100

    Update dependencies and fix 1.16

--- src/cz/jeme/programu/slimechunker/SlimeChunker.java
+++ src/cz/jeme/programu/slimechunker/SlimeChunker.java
@@ -4,13 +4,13 @@ import java.util.Random;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Chunk;
+import org.bukkit.World.Environment;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.World.Environment;
 
 public class SlimeChunker extends JavaPlugin implements Listener {
 
